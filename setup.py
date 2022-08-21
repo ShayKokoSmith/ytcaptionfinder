c78e700413aca12c1885cbb597f6a66ecc4f502c
Shay Koko-Smith <shay.smith314@gmail.com>
2022-08-20 20:43:57 -0400

    Added setup.py Rearranged some things

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ytcaptionfinder',
+    version='0.0.1',
+    author='Shay Koko-Smith',
+    author_email='shay.smith314@gmail.com',
+    description='Finds string in youtube videos',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ShayKokoSmith/ytcaptionfinder',
+    packages=['ytcaptionfinder'],
+    install_requires=['yt_dlp','tblib'],
+)
